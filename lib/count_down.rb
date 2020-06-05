91495e9f11729bfe2b7b2a6c0aab2f395edf22b9
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-05 19:05:47 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,8 @@
 # Write your code here
-count = log10
-while count < 3 do 
-  puts "I am the #{count}, I love to count!"
-  count += 1
+count = 10 
+
+while count > 1 do
+  puts count
+  count -= 1
 end
+puts "Happy New Year!"
